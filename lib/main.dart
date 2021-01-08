f189000f9f3f5c832ba483e3e91189da4d5157a3
LimbSlasher <thelimbslasher@googlemail.com>
2021-01-08 18:22:13 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import './models/global.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ToDo App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      color: Colors.yellow,
+      home: DefaultTabController(
+        length: 3,
+        child: new SafeArea(
+          child: new Scaffold(
+            body: Stack(children: <Widget>[
+              TabBarView(
+                children: [
+                  new Container(
+                    color: darkGreyColor,
+                  ),
+                  new Container(
+                    color: Colors.orange,
+                  ),
+                  new Container(
+                    color: Colors.lightGreen,
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 50),
+                height: 175,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Row(children: <Widget>[
+                  Text("Intray", style: intrayTitleStyle),
+                  Container(),
+                ]),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.only(
+                    top: 120,
+                    left: (MediaQuery.of(context).size.width * 0.5) - 50),
+                child: FloatingActionButton(
+                  child: Icon(Icons.add, size: 100),
+                  backgroundColor: Colors.red,
+                  onPressed: () {},
+                ),
+              )
+            ]),
+            appBar: AppBar(
+              elevation: 0,
+              title: TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.perm_identity),
+                  ),
+                ],
+                labelColor: darkRedColor,
+                unselectedLabelColor: darkGreyColor,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+                indicatorColor: Colors.transparent,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            backgroundColor: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/global.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Colors
+Color darkGreyColor = new Color(0xff212128);
+Color darkRedColor = new Color(0xffDC4F64);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 50);
